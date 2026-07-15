b9e970b6c1ad4f96b6acefa9806912cfd2cc9b61
javart <javartmedia.ads@gmail.com>
2026-07-15 16:51:49 +0700

    Add: database/seeders/DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call([
+            RoleSeeder::class,
+            AdminUserSeeder::class,
+            CategorySeeder::class,
+            BrandSeeder::class,
+            SeriesSeeder::class,
+            SampleProductSeeder::class,
+        ]);
+    }
+}
